ee776ac4829fa98fc330745f1fd2dec21a01b37d
Vaibhav Bhargava <vbharg4@uic.edu>
2025-12-12 11:02:29 -0600

    Removed unused code.

--- IHL_finetune.sh
+++
@@ -1,8 +0,0 @@
-cd /home/aasadi5/Codes/OnlineAdversarialRLHF && \
-source /home/aasadi5/Codes/OnlineAdversarialRLHF/rlhf2/bin/activate && \
-CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node=2 IHL_finetune.py \
-    model_family=phi \
-    split=full \
-    batch_size=4 \
-    gradient_accumulation_steps=4 \
-    lr=1e-5
--- main_process.sh
+++
@@ -1,4 +0,0 @@
-for i in $(seq 1 2); do
-    python data.py config_corruption_mitigation.yaml
-    accelerate launch --config_file ds_zero3.yaml DPO.py config_corruption_mitigation.yaml
-done
